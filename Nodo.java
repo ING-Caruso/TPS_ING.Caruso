0d24959e2aee3f2a2959d60104ec6a4a1b66baa5
ING-Caruso <120569045+ING-Caruso@users.noreply.github.com>
2023-02-15 12:24:21 +0100

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,48 @@
+package lista;
+
+public class Nodo {
+    private int info;
+    private Nodo nodo;
+
+    /**
+     *
+     */
+    public Nodo() {
+        // TODO Auto-generated constructor stub
+        creaNodo(0, null);
+    }
+
+    public Nodo(int info) {
+        this.info = info;
+        nodo = null;
+    }
+
+    private void creaNodo(int i, Nodo link) {
+        this.info = i;
+        this.nodo = link;
+
+        //System.out.println("Nodo n:" + i + " link:" + link + " creato!");
+
+    }
+
+    public void setInfoNodo(int i) {
+        this.info = i;
+    }
+
+    public int getInfoNodo() {
+        return info;
+    }
+
+    public void setLinkNodo(Nodo link) {
+        this.nodo = link;
+    }
+
+    public Nodo getLinkNodo() {
+        return this.nodo;
+    }
+
+    public int visualizza() {
+        return getInfoNodo();
+    }
+
+}
